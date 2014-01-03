ca80779ce270fbfc16f8db1a5daec9a8565908f9
davelooi <davlooi@hotmail.com>
2014-01-03 13:47:43 +0800

    Init
    Initial Commit

---
+++ src/com/oliver/abc/AlphabetHelper.java
@@ -0,0 +1,92 @@
+package com.oliver.abc;
+
+public class AlphabetHelper {
+	public static String getAlphabetWord(String Alphabet)	{
+		String retStr = null;
+		
+		if(Alphabet.equals("A")) {
+			retStr = "Apple";
+		}
+		else if(Alphabet.equals("B")) {
+			retStr = "Bread";
+		}
+		else if(Alphabet.equals("C")) {
+			retStr = "Car";
+		}
+		else if(Alphabet.equals("D")) {
+			retStr = "Dolphin";
+		}
+		else if(Alphabet.equals("E")) {
+			retStr = "Elephant";
+		}
+		else if(Alphabet.equals("F")) {
+			retStr = "Fan";
+		}
+		else if(Alphabet.equals("G")) {
+			retStr = "Grapes";
+		}
+		else if(Alphabet.equals("H")) {
+			retStr = "Helicopter";
+		}
+		else if(Alphabet.equals("I")) {
+			retStr = "Ice Cream";
+		}
+		else if(Alphabet.equals("J")) {
+			retStr = "Jelly";
+		}
+		else if(Alphabet.equals("K")) {
+			retStr = "Key";
+		}
+		else if(Alphabet.equals("L")) {
+			retStr = "Lamp";
+		}
+		else if(Alphabet.equals("M")) {
+			retStr = "Moon";
+		}
+		else if(Alphabet.equals("N")) {
+			retStr = "Necktie";
+		}
+		else if(Alphabet.equals("O")) {
+			retStr = "Ostrich";
+		}
+		else if(Alphabet.equals("P")) {
+			retStr = "Piano";
+		}
+		else if(Alphabet.equals("Q")) {
+			retStr = "Quail";
+		}
+		else if(Alphabet.equals("R")) {
+			retStr = "Rainbow";
+		}
+		else if(Alphabet.equals("S")) {
+			retStr = "Soap";
+		}
+		else if(Alphabet.equals("T")) {
+			retStr = "Tree";
+		}
+		else if(Alphabet.equals("U")) {
+			retStr = "Umbrella";
+		}
+		else if(Alphabet.equals("V")) {
+			retStr = "Violin";
+		}
+		else if(Alphabet.equals("W")) {
+			retStr = "Whistle";
+		}
+		else if(Alphabet.equals("X")) {
+			retStr = "Xylophone";
+		}
+		else if(Alphabet.equals("Y")) {
+			retStr = "Yo-yo";
+		}
+		else if(Alphabet.equals("Z")) {
+			retStr = "Zebra";
+		}
+		else {
+			retStr = "row row row sampan..!!";
+		}
+		
+		return retStr;
+	}
+
+}
---
+++ src/com/oliver/abc/Main.java
@@ -0,0 +1,36 @@
+package com.oliver.abc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class Main extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "com.oliver.abc.ALPHABET";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void onAlphabetButtonClick(View view)
+	{
+		Intent intent = new Intent(this, DisplayAlphabet.class);
+		Button button = (Button) view;
+		String alphabet = button.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, AlphabetHelper.getAlphabetWord(alphabet));
+		startActivity(intent);
+	}
+
+}
